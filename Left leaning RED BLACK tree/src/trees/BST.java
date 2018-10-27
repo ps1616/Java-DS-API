3b62d9b3c7dfe2ed16cc07abf0da610bf44872a5
Priyangi Saxena <priyangisaxena@gmail.com>
2018-10-27 12:07:13 +0530

    adding file

--- Left leaning RED BLACK tree/src/trees/BST.java
+++ Left leaning RED BLACK tree/src/trees/BST.java
@@ -5,6 +5,8 @@ import java.util.Iterator;
 public class BST<K extends Comparable<K>,V> implements Iterable<K> {
 
 	private Node root=null;
+	private final boolean RED=false;
+	private final boolean BLACK=true;
 	
 	public int height()
 	{
@@ -20,7 +22,17 @@ public class BST<K extends Comparable<K>,V> implements Iterable<K> {
 	
 	public void add(K key,V val)
 	{
+		System.out.println("inserting "+ key);
 		root=add(root,key,val);
+		root.color=BLACK;
+		System.out.println("**done**");
+	}
+	
+	private boolean getColor(Node n)
+	{
+		if(n==null)
+			return BLACK;
+		return n.color;
 	}
 	
 	public V get(K key)
@@ -51,8 +63,53 @@ public class BST<K extends Comparable<K>,V> implements Iterable<K> {
 			n.right=add(n.right,key,val);
 		else
 			n.left=add(n.left,key,val);
+		
+		if(getColor(n.left)==BLACK && getColor(n.right)==RED)
+			n=leftRotate(n);
+		
+		if(getColor(n.left)==RED && getColor(n.left.left)==RED)
+			n=rightRotate(n);
+		
+		if(getColor(n.left)==RED && getColor(n.right)==RED)
+			n=swapColor(n);
 		return n;
 	}
+	
+	private Node leftRotate(Node n)
+	{
+		System.out.println("left rotate on "+n.val);
+		Node t=n.right;
+		n.right=t.left;
+		t.left=n;
+		
+		t.color=n.color;
+		n.color=RED;
+		
+		return t;
+	}
+	
+	private Node rightRotate(Node n)
+	{
+		System.out.println("right rotate on "+n.val);
+		Node t=n.left;
+		n.left=t.right;
+		t.right=n;
+		
+		t.color=n.color;
+		n.color=RED;
+		
+		return t;
+	}
+	
+	private Node swapColor(Node n)
+	{
+		System.out.println("color swap on "+n.val);
+		n.left.color=n.color;
+		n.right.color=n.color;
+		n.color=RED;
+		return n;
+	}
+	
 	
 	private class Node{
 	
@@ -60,6 +117,7 @@ public class BST<K extends Comparable<K>,V> implements Iterable<K> {
 		private V val;
 		private Node left=null;
 		private Node right=null;
+		private boolean color;
 		
 		public Node(K key,V val)
 		{
--- Left leaning RED BLACK tree/src/trees/test.java
+++ Left leaning RED BLACK tree/src/trees/test.java
@@ -8,22 +8,24 @@ public class test {
 	{
 		BST<Integer,String> bst=new BST<>();
 		
-		Random r=new Random();
-		for(int i=0;i<100;i++)
-		{
-			bst.add(r.nextInt(100), "someValue");
-		}
-			
-		
-/*		
-		bst.add(5, "five");
 		bst.add(1, "one");
 		bst.add(2, "two");
 		bst.add(5, "five");
+		bst.add(6, "six");
 		bst.add(7, "seven");
+		
+	//	Random r=new Random();
+	/*	for(int i=0;i<100;i++)
+		{
+			bst.add(r.nextInt(100), "someValue");
+		}
+	*/	
+		
+/*		
+		
 */	
-		System.out.println(bst.get(1));
-		System.out.println(bst.get(8));
+	//	System.out.println(bst.get(1));
+	//	System.out.println(bst.get(8));
 		
 		System.out.println(bst.height());
 	}
